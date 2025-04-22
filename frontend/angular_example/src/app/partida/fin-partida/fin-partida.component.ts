daed6b33a833c75ac62ce061fcba86ae97f14e78
Gabriel Coronado <gabopacho13@gmail.com>
2025-04-21 20:20:44 -0500

    Finalizacion de la partida generada

--- frontend/angular_example/src/app/partida/fin-partida/fin-partida.component.ts
+++ frontend/angular_example/src/app/partida/fin-partida/fin-partida.component.ts
@@ -3,21 +3,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
-import { CiudadCaravanaService } from '../../ciudad/ciudad-caravana.service';
-import { CiudadService } from '../../ciudad/ciudad.service';
 import { CaravanaService } from '../../caravana/caravana.service';
 import { PartidaDto } from '../../dto/partida-dto';
 import { CiudadDto } from '../../dto/ciudad-dto';
 import { CaravanaDto } from '../../dto/caravana-dto';
 import { RutaDto } from '../../dto/ruta-dto';
-import { CiudadOrigenService } from '../../ciudad/ciudad-origen.service';
-import { RutaService } from '../../ruta/ruta.service';
-import { CiudadDestinoService } from '../../ciudad/ciudad-destino.service';
 import { forkJoin, of } from 'rxjs';
-import { PartidaCaravanaService } from '../../partida/caravana.service';
 import { PartidaService } from '../../partida/partida.service';
-import { ContadorService } from '../../contador/contador.service';
-import { switchMap, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-fin-partida',
@@ -35,42 +27,38 @@ export class FinPartidaComponent {
     public mensajeConfirmacion: string = '';
     public mostrarPopup: boolean = false;
     public ciudadSeleccionadaParaViajar: any = null;
-    private ciudadEscogida!: CiudadDto;
+    public mensaje: string = '';
     private partidaId!: number;
-    private ciudadId!: number;
     private caravanaId!: number;
-    private rutasIds!: number[];
     constructor(
       private route: ActivatedRoute,
       private partidaService: PartidaService,
-      private ciudadCaravanaService: CiudadCaravanaService,
-      private ciudadOrigenService: CiudadOrigenService,
-      private ciudadDestinoService: CiudadDestinoService,
       private caravanaService: CaravanaService,
-      private ciudadService: CiudadService,
-      private rutaService: RutaService,
-      private partidaCaravanaService: PartidaCaravanaService,
-      private contadorService: ContadorService,
       private router: Router
     ) {}
 
-  ngOnInit(): void {
-    this.partidaId = Number(this.route.snapshot.paramMap.get('idPartida'));
-    this.ciudadId = Number(this.route.snapshot.paramMap.get('idCiudad'));
-    this.caravanaId = Number(this.route.snapshot.paramMap.get('idCaravana'));
-
-    this.partidaService.recuperarPartida(this.partidaId).subscribe(partida => {
-      this.partida = partida;
-    });
-    this.caravanaService.recuperarCaravana(this.caravanaId).subscribe(caravana => {
-      this.caravana = caravana;
-    });
-
-    let mensaje = 'Recolectaste un total de ' + this.caravana.dinero + ' monedas de oro.';
-    if (this.caravana.dinero >= this.partida.gananciaMinima) {
-      mensaje += ' ¡Felicidades, ganaste la partida!';
-    } else {
-      mensaje += ' Haz perdido la partida :(';
+    ngOnInit(): void {
+      this.partidaId = Number(this.route.snapshot.paramMap.get('idPartida'));
+      this.caravanaId = Number(this.route.snapshot.paramMap.get('idCaravana'));
+    
+      forkJoin({
+        partida: this.partidaService.recuperarPartida(this.partidaId),
+        caravana: this.caravanaService.recuperarCaravana(this.caravanaId)
+      }).subscribe(({ partida, caravana }) => {
+        this.partida = partida;
+        this.caravana = caravana;
+    
+        // Armamos el mensaje una vez tenemos todos los datos
+        this.mensaje = `Recolectaste un total de ${caravana.dinero} monedas de oro.`;
+        if (caravana.dinero >= partida.gananciaMinima) {
+          this.mensaje += ' ¡Felicidades, ganaste la partida!';
+        } else {
+          this.mensaje += ' Has perdido la partida :(';
+        }
+      });
     }
-  }
+
+    volverAlInicio(): void {
+      this.router.navigate(['/inicio']); // Cambia la ruta si tu home es distinta
+    }
 }
